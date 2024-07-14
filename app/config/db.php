a8b2cb18c21086e1f65bd3cdcbcc3f8d94760e3a
Klem65 <ilyaklem65@gmail.com>
2024-07-14 17:57:00 +0300

    init

---
+++ app/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql;dbname=promocode',
+    'username' => 'promocode',
+    'password' => 'promocode',
+    'charset' => 'utf8',
+];
