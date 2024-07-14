61528dfbc820fc6df4f7da111b9ca8c3f5d84e82
Klem65 <ilyaklem65@gmail.com>
2024-07-14 22:39:56 +0300

    add create_user_table migration

---
+++ app/migrations/m240714_184800_create_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%users}}`.
+ */
+class m240714_184800_create_users_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%users}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull()->unique(),
+            'access_token' => $this->string()->unique(),
+            'created_at' => $this->integer(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%users}}');
+    }
+}
