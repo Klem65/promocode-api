fcc7f15912e9bf844740422523d0f96d4ab487ce
Klem65 <ilyaklem65@gmail.com>
2024-07-14 22:40:40 +0300

    add User model

---
+++ app/models/User.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string|null $access_token
+ * @property int|null $created_at
+ */
+class User extends ActiveRecord implements IdentityInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username'], 'required'],
+            [['created_at'], 'integer'],
+            [['username', 'access_token'], 'string', 'max' => 255],
+            [['username'], 'unique'],
+            [['access_token'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'access_token' => 'Access Token',
+            'created_at' => 'Created At',
+        ];
+    }
+
+    public static function findIdentity($id)
+    {
+        return static::findOne($id);
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return static::findOne(['access_token' => $token]);
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getAuthKey()
+    {
+        // TODO: Implement getAuthKey() method.
+    }
+
+    public function validateAuthKey($authKey)
+    {
+        // TODO: Implement validateAuthKey() method.
+    }
+}
