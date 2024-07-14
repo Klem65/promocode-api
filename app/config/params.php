5f0bc2ede20ddb01892331c21c808e9dcf38ef5d
Klem65 <ilyaklem65@gmail.com>
2024-07-15 02:01:19 +0300

    add message var

--- app/config/params.php
+++ app/config/params.php
@@ -4,4 +4,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+    'notAvailableMethodMessage' => "The request method is not supported. Please use the POST method to perform this operation."
 ];
