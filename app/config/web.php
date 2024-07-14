5986700c6e434da6213620780e2f9650186e99a5
Klem65 <ilyaklem65@gmail.com>
2024-07-15 02:02:11 +0300

    set up routers

--- app/config/web.php
+++ app/config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'TLgCHOm73OTnTzRi2erBhoe_x6uvee5D',
+            'enableCsrfValidation' => false
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -42,14 +43,14 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                'api/user/create' => 'api/create',
+                'api/promocode/show' => 'api/promocode',
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
